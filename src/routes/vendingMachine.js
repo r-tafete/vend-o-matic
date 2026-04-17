5f112c55bc010a2cac41c1a6b52e3e20970dcb18
Rebecca <rebecca.tafete@gmail.com>
2026-04-16 21:42:49 -0400

    GET /inventory endpoints, router export bug fixed, bevs stocked const for readability

--- src/routes/vendingMachine.js
+++ src/routes/vendingMachine.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-let quarters = 0;
 const inventory = [5, 5, 5] // 3 types of beverages, initially stocked with 5 each
+const NUM_BEVS_STOCKED = inventory.length;
+
+let quarters = 0;
 
 // PUT / (Put 1 quarter in the machine)
 router.put('/', (req, res) => {
@@ -18,4 +20,25 @@ router.delete('/', (req, res) => {
     return res.send();
 });
 
-export default router;
+// GET /inventory (Get stock of all items in machine)
+router.get('/inventory', (req, res) => {
+    return res.status(200).json([...inventory]);
+});
+
+// GET /inventory/:id (Get stock of specified item :id in machine)
+router.get('/inventory/:id', (req, res) => {
+    const bevId = parseInt(req.params.id);
+
+    if (Number.isNaN(bevId) || bevId < 0 || bevId >= NUM_BEVS_STOCKED) {
+        console.warn(`GET /inventory/${req.params.id}: invalid item id`);
+        return res.status(404).json({ error: `Item (ID: ${req.params.id}) not found.` });
+    }
+
+    return res.status(200).json(inventory[bevId]);
+});
+
+// PUT /inventory/:id (Purchase specified item :id from machine)
+
+
+
+module.exports = router;
